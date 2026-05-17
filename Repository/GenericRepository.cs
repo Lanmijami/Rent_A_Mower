0e4145b23ab0daf653d64f48aa12c6fc29e42efe
Lanmijami <milanvukasinovic5@gmail.com>
2026-05-17 20:02:19 +0200

    Finished the Generic Repo and started doing the SystemOperations

--- Repository/GenericRepository.cs
+++ Repository/GenericRepository.cs
@@ -2,6 +2,7 @@
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,64 +12,173 @@ namespace Repository
     public class GenericRepository : IRepository<IEntity>
     {
         private Broker broker = new Broker();
-        public void Add(IEntity obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int AddReturnId(IEntity obj)
-        {
-            throw new NotImplementedException();
-        }
 
         public void BeginTransaction()
         {
-            throw new NotImplementedException();
+            broker.BeginTransaction();
         }
 
         public void CloseConnection()
         {
-            throw new NotImplementedException();
+            broker.CloseConnection();
         }
 
         public void Commit()
         {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(IEntity obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEntity Get(IEntity obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<IEntity> GetAll(IEntity obj)
-        {
-            throw new NotImplementedException();
+            broker?.Commit();
         }
 
         public void OpenConnection()
         {
-            throw new NotImplementedException();
+            broker.OpenConnection();
         }
 
         public void Rollback()
         {
-            throw new NotImplementedException();
+            broker.Rollback();
         }
 
         public List<IEntity> Search(IEntity criteria)
         {
-            throw new NotImplementedException();
+            List<IEntity> result = new List<IEntity>();
+            SqlCommand command = broker.CreateCommand();
+
+            var props = criteria.SearchableProperties;
+            List<string> conditions = new List<string>();
+
+            foreach (var pair in props)
+            {
+                var value = pair.Value;
+
+                if (value == null)
+                    continue;
+
+                if (value is string)
+                {
+                    conditions.Add($"{pair.Key} LIKE @{pair.Key}");
+                    command.Parameters.AddWithValue($"@{pair.Key}", $"%{value}%");
+                }
+                else if (value is DateTime)
+                {
+                    conditions.Add($"{pair.Key} = @{pair.Key}");
+                    command.Parameters.AddWithValue($"@{pair.Key}", value);
+                }
+                else
+                {
+                    conditions.Add($"{pair.Key} = @{pair.Key}");
+                    command.Parameters.AddWithValue($"@{pair.Key}", value);
+                }
+            }
+
+            string whereClause = conditions.Count > 0 ? "WHERE " + string.Join(" AND ", conditions) : "";
+            command.CommandText = $"SELECT * FROM {criteria.TableName} {whereClause}";
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    IEntity rowObject = criteria.ReadObjectRow(reader);
+                    result.Add(rowObject);
+                }
+            }
+
+            return result;
         }
 
         public void Update(IEntity obj)
         {
-            throw new NotImplementedException();
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"UPDATE {obj.TableName} SET {obj.UpdateStatement} WHERE {obj.Condition}";
+
+            foreach (var param in obj.Parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
+
+        public void Delete(IEntity obj)
+        {
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"DELETE FROM {obj.TableName} WHERE {obj.Condition}";
+
+            foreach (var param in obj.Parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
+
+        public IEntity Get(IEntity obj)
+        {
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"SELECT * FROM {obj.TableName} WHERE {obj.Condition}";
+
+            foreach (var param in obj.Parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return obj.ReadObjectRow(reader);
+                }
+            }
+            return null;
+        }
+
+        public List<IEntity> GetAll(IEntity obj)
+        {
+            List<IEntity> result = new List<IEntity>();
+            //var result = new List<IEntity>();
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"SELECT * FROM {obj.TableName}";
+
+            using (SqlDataReader reader = command.ExecuteReader()) 
+            {
+                while (reader.Read())
+                {
+                    IEntity rowObject = obj.ReadObjectRow(reader);
+                    result.Add(rowObject);
+                }
+            }
+            command.Dispose();
+            return result;
+        }
+
+        public void Add(IEntity obj)
+        {
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"INSERT INTO {obj.TableName} ({obj.InsertColumns}) VALUES ({obj.InsertParameters})";
+
+            foreach (var param in obj.Parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
+
+        public int AddReturnId(IEntity obj)
+        {
+            SqlCommand command = broker.CreateCommand();
+            command.CommandText = $"INSERT INTO {obj.TableName} ({obj.InsertColumns}) VALUES ({obj.InsertParameters}); SELECT SCOPE_IDENTITY();";
+
+            foreach (var param in obj.Parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            int result = Convert.ToInt32(command.ExecuteScalar());
+            command.Dispose();
+            return result;
         }
     }
 }
---
+++ SystemOperations/SystemOperationBase.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Repository;
+
+namespace SystemOperations
+{
+    public abstract class SystemOperationBase
+    {
+        protected IRepository<IEntity> repository = new GenericRepository();
+        public void ExecuteTemplate()
+        {
+            try
+            {
+                repository.OpenConnection();
+                repository.BeginTransaction();
+                Execute();
+                repository.Commit();
+            }
+            catch(Exception ex)
+            {
+                repository.Rollback();
+                throw;
+            }
+            finally
+            {
+                repository.CloseConnection();
+            }
+        }
+
+        protected abstract void Execute();
+    }
+}
